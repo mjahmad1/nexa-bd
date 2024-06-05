526a4fd388513c7b165f85471312ad5db1d607db
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2024-06-05 14:08:40 +0000

    Initial commit

---
+++ src/pages/1st Tr.tt5pa.js
@@ -0,0 +1,89 @@
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+    // Write your Javascript code here using the Velo framework API
+
+    // Print hello world:
+    // console.log("Hello world!");
+
+    // Call functions on page elements, e.g.:
+    // $w("#button1").label = "Click me!";
+
+    // Click "Run", or Preview your site, to execute your code
+
+});
+
+/**
+*	Adds an event handler that runs when the element is clicked.
+	[Read more](https://www.wix.com/corvid/reference/$w.ClickableMixin.html#onClick)
+*	 @param {$w.MouseEvent} event
+*/
+export function classicSection1_click(event) {
+    // This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+    // Add your code for this event here: 
+}
+
+/**
+*	Adds an event handler that runs when the element is double-clicked.
+	[Read more](https://www.wix.com/corvid/reference/$w.ClickableMixin.html#onDblClick)
+*	 @param {$w.MouseEvent} event
+*/
+export function classicSection1_dblClick(event) {
+    // This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+    // Add your code for this event here: 
+}
+
+/**
+*	Adds an event handler that runs when the pointer is moved
+ onto the element.
+
+ You can also [define an event handler using the Properties and Events panel](https://support.wix.com/en/article/velo-reacting-to-user-actions-using-events).
+	[Read more](https://www.wix.com/corvid/reference/$w.Element.html#onMouseIn)
+*	 @param {$w.MouseEvent} event
+*/
+export function classicSection1_mouseIn(event) {
+    // This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+    // Add your code for this event here: 
+}
+
+/**
+*	Adds an event handler that runs when the pointer is moved
+ off of the element.
+
+ You can also [define an event handler using the Properties and Events panel](https://support.wix.com/en/article/velo-reacting-to-user-actions-using-events).
+	[Read more](https://www.wix.com/corvid/reference/$w.Element.html#onMouseOut)
+*	 @param {$w.MouseEvent} event
+*/
+export function classicSection1_mouseOut(event) {
+    // This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+    // Add your code for this event here: 
+}
+
+/**
+*	Adds an event handler that runs when an element is displayed
+ in the viewable part of the current window.
+	[Read more](https://www.wix.com/corvid/reference/$w.ViewportMixin.html#onViewportEnter)
+*	 @param {$w.Event} event
+*/
+export function classicSection1_viewportEnter(event) {
+    // This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+    // Add your code for this event here: 
+}
+
+/**
+*	Adds an event handler that runs when an element is no longer
+ displayed in the viewable part of the current window.
+	[Read more](https://www.wix.com/corvid/reference/$w.ViewportMixin.html#onViewportLeave)
+*	 @param {$w.Event} event
+*/
+export function classicSection1_viewportLeave(event) {
+    // This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+    // Add your code for this event here: 
+}
+.section {
+    width: 90 % ;
+    border - top: 5 px solid black;
+    padding - top: 2 rem;
+    margin - top: 2 rem;
+}
---
+++ src/pages/masterPage.js
@@ -0,0 +1,51 @@
+// The code in this file will load on every page of your site
+
+$w.onReady(function () {
+	// Write your code here
+});
+
+/**
+*	Adds an event handler that runs when the element is clicked.
+	[Read more](https://www.wix.com/corvid/reference/$w.ClickableMixin.html#onClick)
+*	 @param {$w.MouseEvent} event
+*/
+export function header1_click(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+}
+
+/**
+*	Adds an event handler that runs when the element is double-clicked.
+	[Read more](https://www.wix.com/corvid/reference/$w.ClickableMixin.html#onDblClick)
+*	 @param {$w.MouseEvent} event
+*/
+export function header1_dblClick(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+}
+
+/**
+*	Adds an event handler that runs when the pointer is moved
+ onto the element.
+
+ You can also [define an event handler using the Properties and Events panel](https://support.wix.com/en/article/velo-reacting-to-user-actions-using-events).
+	[Read more](https://www.wix.com/corvid/reference/$w.Element.html#onMouseIn)
+*	 @param {$w.MouseEvent} event
+*/
+export function header1_mouseIn(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+}
+
+/**
+*	Adds an event handler that runs when the pointer is moved
+ off of the element.
+
+ You can also [define an event handler using the Properties and Events panel](https://support.wix.com/en/article/velo-reacting-to-user-actions-using-events).
+	[Read more](https://www.wix.com/corvid/reference/$w.Element.html#onMouseOut)
+*	 @param {$w.MouseEvent} event
+*/
+export function header1_mouseOut(event) {
+	// This function was added from the Properties & Events panel. To learn more, visit http://wix.to/UcBnC-4
+	// Add your code for this event here: 
+}
